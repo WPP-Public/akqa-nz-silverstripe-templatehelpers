a8b67ba836b020268590bb795767ecf4e96c3589
Will Rossiter <will.rossiter@akqa.com>
2023-07-19 14:09:10 +1200

    fix: remove composer dependancy

--- src/TemplateHelpers.php
+++ src/TemplateHelpers.php
@@ -107,6 +107,6 @@ class TemplateHelpers implements TemplateGlobalProvider
      */
     public static function joinPaths(array $paths)
     {
-        return preg_replace('#/+#','/', join('/', $paths));
+        return preg_replace('#/+#', '/', join('/', $paths));
     }
 }
